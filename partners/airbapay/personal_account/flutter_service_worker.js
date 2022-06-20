66980975fa5f0402815f8a02d004c63c1417d31b
Alexander Yemelyanov <alex@justprodev.com>
2022-06-21 05:22:29 +0600

    release airba personal account test

--- partners/airbapay/personal_account/flutter_service_worker.js
+++ partners/airbapay/personal_account/flutter_service_worker.js
@@ -4,9 +4,9 @@ const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 const RESOURCES = {
   "version.json": "13b821e2f8981b4bcc852de77464438e",
-"index.html": "a42df51b811856d7bd8a7bbd0c8ce77f",
-"/": "a42df51b811856d7bd8a7bbd0c8ce77f",
-"main.dart.js": "d12eec8d5508e3bc7962a582a921f123",
+"index.html": "1c451d58fd033f8656acfd355973ccaa",
+"/": "1c451d58fd033f8656acfd355973ccaa",
+"main.dart.js": "99b96cc882ccfc8312bd9df4e4796c05",
 "flutter.js": "eb2682e33f25cd8f1fc59011497c35f8",
 "favicon.png": "3b97dc6e34ad7342f9f62143accee513",
 "icons/Icon-192.png": "18a69fa304b37b41cf51e641501c95ca",
@@ -15,7 +15,7 @@ const RESOURCES = {
 "icons/Icon-512.png": "0680334db44e14c6aa91fa7706dbc685",
 "manifest.json": "6f40054170ed3443f7721bc8314b8482",
 "assets/AssetManifest.json": "1c15db766faa42e7269fbe74bfe6071b",
-"assets/NOTICES": "dea8ce6788f03c378a9ba8f873b8fc02",
+"assets/NOTICES": "8398d52708c8a0de8e8224822a9ab8f3",
 "assets/FontManifest.json": "48143ce3438bbf984acc621c83a2da55",
 "assets/packages/cupertino_icons/assets/CupertinoIcons.ttf": "6d342eb68f170c97609e9da345464e5e",
 "assets/fonts/semi_bold.ttf": "c641dbee1d75892e4d88bdc31560c91b",
